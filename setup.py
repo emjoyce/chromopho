55d13e5fc26a76efb40790250749a416fa139af1
Emily Joyce <emilyjoyce@169-231-217-167.wireless.ucsb.edu>
2025-02-03 22:28:14 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "chromopho",
+    version = "0.0.1",
+    author_email = "emjoyce@ucsb.edu",
+    description = "a simulation of bipolar subtypes as color filters",
+    url = "https://github.com/emjoyce/chromopho.git",
+    packages = find_packages(),
+    install_requires = [
+        line.strip() for line in open("requirements.txt", "r") if line.strip() and not line.startswith("#")
+        ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = ">=3.6",
+)
+
