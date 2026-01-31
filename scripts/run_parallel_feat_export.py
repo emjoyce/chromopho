85097298470b9970991b9742990a993f754699ee
Emily Joyce <emilybemily55@gmail.com>
2026-01-30 19:21:57 -0800

    cleaning up

--- scripts/run_parallel_feat_export.py
+++ scripts/run_parallel_feat_export.py
@@ -13,32 +13,39 @@ from chromopho.bipolar_image import BipolarImageProcessor
 # -------------------------
 
 # --- Mosaic / subtype parameters (used by build_mosaic) ---
-SEED = 10
+SEED = 0
 N_CELLS = 25000
 SHAPE = "circle"
 
 # receptive field / subtype tuning
 ALPHA_CENTER = 1.0
-ALPHA_SURROUND_S = 0.25
-ALPHA_SURROUND_LM = 0.65
-ALPHA_SURROUND_DIFF = 0.45
 
-CENTER_SIGMA_S = 8.0
-SURROUND_SIGMA_S = 25.0
-CENTER_SIGMA_LM = 3.0
-SURROUND_SIGMA_LM = 10.0
-CENTER_SIGMA_DIFF = 6.0
-SURROUND_SIGMA_DIFF = 8.0
+ALPHA_SURROUND_S = 0.5
+ALPHA_SURROUND_LM = 0.9
+ALPHA_SURROUND_DIFF = 1.3
 
-REC_KIND = "softplus"
-REC_R0 = 0.1
-REC_ALPHA = 0.05
-REC_BETA = 5
+CENTER_SIGMA_S = 9.0
+SURROUND_SIGMA_S = CENTER_SIGMA_S*9
+CENTER_SIGMA_LM = 7.0
+SURROUND_SIGMA_LM = CENTER_SIGMA_LM*9
+CENTER_SIGMA_DIFF = 18.0
+SURROUND_SIGMA_DIFF = CENTER_SIGMA_DIFF*9
+
+APPLY_RECTIFICATION = True
+ON_K = 0.7
+ON_N = 2.0
+OFF_K = 0.7
+OFF_N = 1.5
+
+NONLIN_ADAPT_CONES = True
+
+
 
 # --- Processing / output parameters (used by process_one_image) ---
-DEFAULT_GAUSSIAN_BLUR = False
-DEFAULT_GAUSSIAN_SIGMA = 1.0
 SAVE_DTYPE = "float32"  # dtype for saved .npy arrays
+
+# optional amacrine blur applied to bipolar cell outputs (None -> disabled)
+AMACRINE_SIGMA_BLUR = None
 
 # --- Parallel / runtime parameters (used by main) ---
 DEFAULT_WORKERS = 10   # explicit CLI override if set # conservative - i have 12 performance cores
@@ -59,14 +66,34 @@ def choose_default_workers(default=DEFAULT_WORKERS, max_cap=MAX_DEFAULT_WORKERS,
     return n_use
 
 # --- I/O defaults (can be overridden by CLI) ---
-DEFAULT_IMAGE_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/imgs/to_do_natural_400'
-DEFAULT_OUTPUT_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/imgs/grid_output_old_params' 
-DEFAULT_DONE_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/imgs/done_2'
+# DEFAULT_IMAGE_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/imgs/to_do_natural_400'
+# DEFAULT_OUTPUT_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/imgs/grid_output_old_params' 
+# DEFAULT_DONE_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/imgs/done_2'
 
 # test
 # DEFAULT_IMAGE_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/to_do'
 # DEFAULT_OUTPUT_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/grid_output_old_params' 
 # DEFAULT_DONE_DIR = '/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/done'
+
+# sfn mini
+# DEFAULT_IMAGE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/sfn_mini/to_do"
+# DEFAULT_OUTPUT_DIR = "//Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/sfn_mini/feats"
+# DEFAULT_DONE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/sfn_mini/done"
+
+# sfn mini test images
+# DEFAULT_IMAGE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/sfn_mini/to_do"
+# DEFAULT_OUTPUT_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/sfn_mini/feats"
+# DEFAULT_DONE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/test_imgs/sfn_mini/done"
+
+# sfn full 
+# DEFAULT_IMAGE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/imgs/done"
+# DEFAULT_OUTPUT_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/imgs/sfn_full_feats"
+# DEFAULT_DONE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/imgs/done_2"
+
+# solid imgs sfn
+DEFAULT_IMAGE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/imgs/solid_color_manifold_imgs"
+DEFAULT_OUTPUT_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/imgs/sfn_manifold_feats"
+DEFAULT_DONE_DIR = "/Users/emilyjoyce/repos/chromopho/chromopho/imgs/done"
 
 # -------------------------
 # end CONFIGURATION
@@ -79,7 +106,7 @@ def init_worker(m):
     global _WORK_MOSAIC
     _WORK_MOSAIC = m
 
-def process_one_image(img_path, output_dir, analysis_complete_dir, gaussian_blur, gaussian_sigma, verbose):
+def process_one_image(img_path, output_dir, analysis_complete_dir, amacrine_sigma_blur, verbose):
     try:
         img = plt.imread(img_path)
     except Exception:
@@ -87,10 +114,9 @@ def process_one_image(img_path, output_dir, analysis_complete_dir, gaussian_blur
         img = imread(img_path)
 
     # create BipolarImageProcessor and compute per-cell flattened outputs
-    bip = BipolarImageProcessor(_WORK_MOSAIC, img, save_flat = True)
+    # pass optional amacrine blur into the processor (None disables it)
+    bip = BipolarImageProcessor(_WORK_MOSAIC, img, save_flat = True, amacrine_sigma_blur=amacrine_sigma_blur)
     grid_outputs = bip.grid_outputs
-    if gaussian_blur is not False:
-        grid_outputs = gaussian_filter(grid_outputs, sigma=gaussian_sigma)
 
     # save results
     filename_base = os.path.splitext(os.path.basename(img_path))[0]
@@ -128,27 +154,48 @@ def build_mosaic():
     center_sigma_diff = CENTER_SIGMA_DIFF
     surround_sigma_diff = SURROUND_SIGMA_DIFF
 
-    rec_kind = REC_KIND
-    rec_r0 = REC_R0
-    rec_alpha = REC_ALPHA
-    rec_beta = REC_BETA
+    nonlin_adapt_cones = NONLIN_ADAPT_CONES
+    apply_rectification = APPLY_RECTIFICATION
 
-    s_rf_params = {'center_sigma':center_sigma_s, 'surround_sigma':surround_sigma_s, 'alpha_center':1, 'alpha_surround':alpha_surround_s, 'rec_kind':rec_kind, 'rec_beta':rec_beta}
-    s_off = mosaic_mod.BipolarSubtype(name = 's_off', ratio = 2.5/100,  rf_size = 15, color_filter_params = {'center':'-s', 'surround':'+lm'}, rf_params=s_rf_params)
-    s_on  = mosaic_mod.BipolarSubtype(name = 's_on',  ratio = 2.5/100,  rf_size = 15, color_filter_params = {'center':'+s', 'surround':'-lm'}, rf_params=s_rf_params)
+    on_k = ON_K
+    on_n = ON_N
+    off_k = OFF_K
+    off_n = OFF_N
 
-    lm_rf_params = {'center_sigma':center_sigma_lm, 'surround_sigma':surround_sigma_lm, 'alpha_center':1, 'alpha_surround':alpha_surround_lm, 'rec_kind':rec_kind, 'rec_beta':rec_beta}
-    m_on  = mosaic_mod.BipolarSubtype(name = 'm_on',  ratio = 18/100, rf_size = 6,  color_filter_params = {'center':'+m', 'surround':'-l'}, rf_params=lm_rf_params)
-    m_off = mosaic_mod.BipolarSubtype(name = 'm_off', ratio = 18/100, rf_size = 6,  color_filter_params = {'center':'-m', 'surround':'+l'}, rf_params=lm_rf_params)
+    s_ratio = 3/100
+    lm_ratio = 15/100
+    dif_ratio = 15.5/100
 
-    l_on  = mosaic_mod.BipolarSubtype(name = 'l_on',  ratio = 18/100, rf_size = 6,  color_filter_params = {'center':'+l', 'surround':'-m'}, rf_params=lm_rf_params)
-    l_off = mosaic_mod.BipolarSubtype(name = 'l_off', ratio = 19/100, rf_size = 6,  color_filter_params = {'center':'-l', 'surround':'+m'}, rf_params=lm_rf_params)
+    rec_frac = .15
+    rf_size_s = 2+int(np.floor(rec_frac*surround_sigma_s))
+    rf_size_lm = int(np.floor(rec_frac*surround_sigma_lm))
+    rf_size_diff = int(np.floor(rec_frac*surround_sigma_diff))
 
-    dif_rf_params = {'center_sigma':center_sigma_diff, 'surround_sigma':surround_sigma_diff, 'alpha_center':1, 'alpha_surround':alpha_surround_diff, 'rec_kind':rec_kind, 'rec_beta': rec_beta}
-    dif_on  = mosaic_mod.BipolarSubtype(name = 'dif_on',  ratio = 10/100, rf_size = 8, color_filter_params = {'center':'+lm', 'surround':'-lm'})
-    dif_off = mosaic_mod.BipolarSubtype(name = 'dif_off', ratio = 10/100, rf_size = 8, color_filter_params = {'center':'-lm', 'surround':'+lm'})
 
-    mosaic = mosaic_mod.BipolarMosaic(N_CELLS, shape = SHAPE, subtypes = [m_off, m_on, l_off, l_on, s_off, s_on, dif_on, dif_off])
+
+    s_rf_params = {'center_sigma':center_sigma_s, 'surround_sigma':surround_sigma_s, 'alpha_center':alpha_center, 
+                            'alpha_surround':alpha_surround_s,  'apply_rectification':apply_rectification, 
+                            'on_k':on_k, 'on_n':on_n, 'off_k':off_k, 'off_n':off_n, 'nonlin_adapt_cones': nonlin_adapt_cones}
+    s_off = mosaic_mod.BipolarSubtype(name = 's_off', ratio = s_ratio,  rf_size = rf_size_s, color_filter_params = {'center':'-s', 'surround':'+lm'}, rf_params=s_rf_params)
+    s_on  = mosaic_mod.BipolarSubtype(name = 's_on',  ratio = s_ratio,  rf_size = rf_size_s, color_filter_params = {'center':'+s', 'surround':'-lm'}, rf_params=s_rf_params)
+
+
+    lm_rf_params = {'center_sigma':center_sigma_lm, 'surround_sigma':surround_sigma_lm, 'alpha_center':alpha_center, 
+                    'alpha_surround':alpha_surround_lm, 'apply_rectification':apply_rectification, 
+                            'on_k':on_k, 'on_n':on_n, 'off_k':off_k, 'off_n':off_n, 'nonlin_adapt_cones': nonlin_adapt_cones}
+    m_on  = mosaic_mod.BipolarSubtype(name = 'm_on',  ratio = lm_ratio, rf_size = rf_size_lm,  color_filter_params = {'center':'+m', 'surround':'-lm'}, rf_params=lm_rf_params)
+    m_off = mosaic_mod.BipolarSubtype(name = 'm_off', ratio = lm_ratio, rf_size = rf_size_lm,  color_filter_params = {'center':'-m', 'surround':'+lm'}, rf_params=lm_rf_params)
+    l_on  = mosaic_mod.BipolarSubtype(name = 'l_on',  ratio = lm_ratio, rf_size = rf_size_lm,  color_filter_params = {'center':'+l', 'surround':'-lm'}, rf_params=lm_rf_params)
+    l_off = mosaic_mod.BipolarSubtype(name = 'l_off', ratio = lm_ratio, rf_size = rf_size_lm,  color_filter_params = {'center':'-l', 'surround':'+lm'}, rf_params=lm_rf_params)
+
+    dif_rf_params = {'center_sigma':center_sigma_diff, 'surround_sigma':surround_sigma_diff, 'alpha_center':alpha_center, 
+                        'alpha_surround':alpha_surround_diff, 'apply_rectification':apply_rectification, 
+                            'on_k':on_k, 'on_n':on_n, 'off_k':off_k, 'off_n':off_n, 'nonlin_adapt_cones': nonlin_adapt_cones}
+
+    dif_on  = mosaic_mod.BipolarSubtype(name = 'dif_on',  ratio = dif_ratio, rf_size = rf_size_diff, color_filter_params = {'center':'+lm', 'surround':'-lm'}, rf_params=dif_rf_params)
+    dif_off = mosaic_mod.BipolarSubtype(name = 'dif_off', ratio = dif_ratio, rf_size = rf_size_diff, color_filter_params = {'center':'-lm', 'surround':'+lm'}, rf_params=dif_rf_params)
+
+    mosaic = mosaic_mod.BipolarMosaic(N_CELLS, shape = SHAPE, subtypes = [m_off, m_on, l_off, l_on, s_off, s_on, dif_off, dif_on])
     return mosaic
 
 def main():
@@ -157,8 +204,8 @@ def main():
     parser.add_argument('--output_dir', default=DEFAULT_OUTPUT_DIR)
     parser.add_argument('--done_dir', default=DEFAULT_DONE_DIR)
     parser.add_argument('--workers', type=int, default=DEFAULT_WORKERS)
-    parser.add_argument('--gaussian_blur', action='store_true', default=DEFAULT_GAUSSIAN_BLUR)
-    parser.add_argument('--gaussian_sigma', type=float, default=DEFAULT_GAUSSIAN_SIGMA)
+    parser.add_argument('--amacrine_sigma_blur', type=float, default=AMACRINE_SIGMA_BLUR,
+                        help='Optional masked amacrine blur applied to output of bipolar cells (None disables)')
     parser.add_argument('--verbose', action='store_true', default=False)
     args = parser.parse_args()
 
@@ -174,7 +221,7 @@ def main():
     workers = args.workers if (args.workers and args.workers > 0) else choose_default_workers()
 
     with ProcessPoolExecutor(max_workers=workers, initializer=init_worker, initargs=(mosaic,)) as ex:
-        futures = {ex.submit(process_one_image, p, args.output_dir, args.done_dir, args.gaussian_blur, args.gaussian_sigma, args.verbose): p for p in img_paths}
+        futures = {ex.submit(process_one_image, p, args.output_dir, args.done_dir, args.amacrine_sigma_blur, args.verbose): p for p in img_paths}
         for fut in as_completed(futures):
             p = futures[fut]
             try:
